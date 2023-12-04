9222878d3e5136e91eb1b9dec099e4e5a7f69611
Bill Jones <unrealguard57@hotmail.com>
2023-12-04 10:57:59 -0500

    doc(rust): [rust-book] refactor parse logic

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 Collection of tutorials and projects to help learn
 the rust programming language.
 
+
+## Environment Setup
+
+### Install rustup toolchain
+```bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+```
+
+### Install the necessary compiler tools
+```bash
+sudo apt-get install gcc
+```
--- rust-book/minigrep/src/main.rs
+++ rust-book/minigrep/src/main.rs
@@ -3,15 +3,28 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for value: {}", query);
-    println!("In path: {}", file_path);
+    println!("Searching for value: {}", config.query);
+    println!("In path: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("With file contents:\n{contents}");
-    
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &Vec<String>) -> Config {
+    let query: &String = &args[1];
+    let file_path: &String = &args[2];
+
+    Config {
+        query: query.clone(),
+        file_path: file_path.clone(),
+    }
 }
