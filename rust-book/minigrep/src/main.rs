9bc1b709f87cc9bdf2c18ef50b420e1f9375b52d
Bill Jones <unrealguard57@hotmail.com>
2023-12-04 11:14:45 -0500

    doc(rust): [rust-book] Better error handling

--- rust-book/minigrep/src/main.rs
+++ rust-book/minigrep/src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for value: {}", config.query);
     println!("In path: {}", config.file_path);
@@ -19,12 +22,18 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &Vec<String>) -> Config {
-    let query: &String = &args[1];
-    let file_path: &String = &args[2];
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Expected at least 3 arguments");
+        }
 
-    Config {
-        query: query.clone(),
-        file_path: file_path.clone(),
+        let query: &String = &args[1];
+        let file_path: &String = &args[2];
+
+        Ok(Config {
+            query: query.clone(),
+            file_path: file_path.clone(),
+        })
     }
 }
