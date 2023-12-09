ab98295cbbfca55d195db2dad31b14444fcc83b8
William Jones <unrealguard57@hotmail.com>
2023-12-09 20:13:31 +0000

    feat(dioxus): Styled story listing

--- dioxus/web/src/components/story_listing.rs
+++ dioxus/web/src/components/story_listing.rs
@@ -1,15 +1,15 @@
 #![allow(non_snake_case)]
-use chrono::{DateTime, Utc};
+use chrono::{format::format, DateTime, Utc};
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct StoryPageData {
     #[serde(flatten)]
     pub item: StoryItem,
     #[serde(default)]
-    pub comments: Vec<Comment>
+    pub comments: Vec<Comment>,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -28,9 +28,8 @@ pub struct StoryItem {
     pub time: DateTime<Utc>,
     #[serde(default)]
     pub kids: Vec<i64>,
-    pub r#type: String
+    pub r#type: String,
 }
-
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Comment {
@@ -52,6 +51,7 @@ pub struct Comment {
 pub fn StoryListing(cx: Scope, story: StoryItem) -> Element {
     let StoryItem {
         title,
+        url,
         by,
         score,
         time,
@@ -59,13 +59,65 @@ pub fn StoryListing(cx: Scope, story: StoryItem) -> Element {
         ..
     } = story;
 
-    let comments = kids.len();
+    let url = url.as_deref().unwrap_or_default();
+    let hostname = url
+        .trim_start_matches("https://")
+        .trim_start_matches("http://")
+        .trim_start_matches("www.");
+    let score = format!(
+        "{score} {}",
+        if *score == 1 { " points" } else { " points" }
+    );
+
+    let comments = format!(
+        "{} {}",
+        kids.len(),
+        if kids.len() == 1 {
+            " comment"
+        } else {
+            " comments"
+        }
+    );
+
+    let time = time.format("%D %l:%M %p");
 
     cx.render(rsx! {
         div {
             padding: "-1.5rem",
             position: "relative",
-            "{title} {by} {score} {time} {comments}"
-        }       
+            div {
+                font_size: "1.5rem",
+                a {
+                    href: url,
+                    "{title}"
+                }
+                a {
+                    color: "gray",
+                    href: "https://news.ycombinator.com/from?site={hostname}",
+                    text_decoration: "none",
+                    " ({hostname})"
+                }
+            }
+            div {
+                display: "flex",
+                flex_direction: "row",
+                color: "gray",
+                div {
+                    "{score}"
+                }
+                div {
+                    padding_left: "0.5rem",
+                    "by {by}"
+                }
+                div {
+                    padding_left: "0.5rem",
+                    "{time}"
+                },
+                div {
+                    padding_left: "0.5rem",
+                    "{comments}"
+                }
+            }
+        }
     })
 }
