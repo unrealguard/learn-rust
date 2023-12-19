fd56539d62ff892b704276a222550ce496f55a21
William Jones <unrealguard57@hotmail.com>
2023-12-19 19:55:02 +0000

    feat(dioxus): Get story comments

--- dioxus/core/src/hackernews_adatper/api.rs
+++ dioxus/core/src/hackernews_adatper/api.rs
@@ -1,6 +1,6 @@
 use futures_util::future::join_all;
 
-use crate::components::story_listing::StoryItem;
+use crate::components::{comment::Comment, story_listing::StoryItem};
 
 pub static BASE_API_URL: &str = "https://hacker-news.firebaseio.com/v0/";
 pub static ITEM_API: &str = "item/";
@@ -30,4 +30,30 @@ pub async fn get_stories(
         .collect();
 
     Ok(stories)
+}
+
+#[async_recursion::async_recursion(?Send)]
+pub async fn get_comment_with_depth(
+    id: i64,
+    depth: i64,
+) -> Result<Comment, reqwest::Error> {
+    let url = format!("{}{}{}.json", BASE_API_URL, ITEM_API, id);
+    let mut comment = reqwest::get(&url).await?.json::<Comment>().await?;
+    if depth > 0 {
+        let sub_comments_futures = comment
+            .kids
+            .iter()
+            .map(|story_id| get_comment_with_depth(*story_id, depth - 1));
+        comment.sub_comments = join_all(sub_comments_futures)
+            .await
+            .into_iter()
+            .filter_map(|c| c.ok())
+            .collect();
+    }
+    Ok(comment)
+}
+
+pub async fn get_comment(comment_id: i64) -> Result<Comment, reqwest::Error> {
+    let comment = get_comment_with_depth(comment_id, COMMENT_DEPTH).await?;
+    Ok(comment)
 }
--- dioxus/core/tests/apis.rs
+++ dioxus/core/tests/apis.rs
@@ -1,4 +1,4 @@
-use core::hackernews_adatper::api::get_stories;
+use core::hackernews_adatper::api::{get_stories, get_comment};
 
 #[test]
 fn get_stories_works() {
@@ -9,5 +9,20 @@ fn get_stories_works() {
         let number_of_stories = stories.unwrap().len();
         println!("Number of stories: {}", number_of_stories);
         assert!(number_of_stories > 0);
+    });
+}
+
+#[test]
+fn get_comment_works() {
+    tokio_test::block_on(async {
+        let comment_id = 38698579;
+        let comment = get_comment(comment_id).await;
+        if let Err(error) = &comment {
+            println!("Encountered an error: {}", error);
+        }
+        assert!(comment.is_ok());
+
+        let result_comment_id = comment.unwrap().id;
+        assert_eq!(comment_id, result_comment_id)
     });
 }
